46484280c4cb0374d381192faff91a36a27056cf
KSY <xaesoo30@gmail.com>
2025-06-20 21:15:44 +0900

    [Add] 점프 및 애니메이션, 애니메이터 작업

--- Assets/02. Script/Knight Controller.cs
+++ Assets/02. Script/Knight Controller.cs
@@ -8,14 +8,19 @@ namespace Middle_Age_2D_Game
         [SerializeField] protected float moveSpeed = 7f;
         [SerializeField] protected float jumpPower = 7f;
 
-        private GameObject gameObj;
+        private GameObject knightOb;
         private Rigidbody2D knightRb;
+        private Animator knightAnim;
+
+        public bool isGround = false;
+        private float x, y; // X, Y축 값을 넣기위한 변수
         //private SpriteRenderer spriteRd;
 
         public void Start()
         {
-            gameObj = new GameObject();
-            knightRb = gameObject.AddComponent<Rigidbody2D>();
+            knightOb = new GameObject();
+            knightRb = GetComponent<Rigidbody2D>();
+            knightAnim = GetComponent<Animator>();
         }
 
         public void Update()
@@ -24,24 +29,53 @@ namespace Middle_Age_2D_Game
             Jump();
         }
 
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.collider.CompareTag("Ground"))
+            {
+                isGround = true;
+            }
+        }
+
+        void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.collider.CompareTag("Ground"))
+            {
+                isGround = false;
+            }
+        }
+
         /// <summary>
         /// 좌우만 움직이는 기능
         /// </summary>
         private void Move()
         {
-            float x = Input.GetAxis("Horizontal"); // X축 이동
+            x = Input.GetAxis("Horizontal"); // X축 이동
             transform.position += new Vector3(x, 0, 0) * moveSpeed * Time.deltaTime;
         }
-        
+
         /// <summary>
         /// 점프하는 기능
         /// </summary>
         private void Jump()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
             {
-                knightRb.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
+                 knightRb.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
             }
+        }
+
+        /// <summary>
+        /// 공격 기능
+        /// </summary>
+        void Attack()
+        {
+
+        }
+
+        private void AnimIdlePlay()
+        {
+
         }
     }
 }
