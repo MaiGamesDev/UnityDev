f6e6bdfe6c86d6d9fab1cde48031456a74ccf07f
KSY <xaesoo30@gmail.com>
2025-06-23 00:06:41 +0900

    공격 애니메이션 구현
    공격 프레임별 콜라이더 구현

--- Assets/02. Script/Knight Controller.cs
+++ Assets/02. Script/Knight Controller.cs
@@ -6,7 +6,8 @@ namespace Middle_Age_2D_Game
 {
     public class KnightController : MonoBehaviour, IMoveMethod
     {
-        private int hp = 1;
+        private float knightHP = 1;
+        private float damage = 1;
 
         [SerializeField] protected float moveSpeed = 7f;
         [SerializeField] protected float jumpPower = 7f;
@@ -14,8 +15,9 @@ namespace Middle_Age_2D_Game
         private GameObject knightOb;
         private Rigidbody2D knightRb;
         private Animator knightAnim;
+        private SpriteRenderer knightSR;
 
-        public bool isGround = false;
+        private bool isGround = false;
         private float x; // X축 값을 넣기위한 변수
         //private SpriteRenderer spriteRd;
 
@@ -24,12 +26,14 @@ namespace Middle_Age_2D_Game
             knightOb = new GameObject();
             knightRb = GetComponent<Rigidbody2D>();
             knightAnim = GetComponent<Animator>();
+            knightSR = GetComponent<SpriteRenderer>();
         }
 
         public void Update()
         {
             StartCoroutine(Run());
             Jump();
+            Attack();
         }
 
         void OnCollisionEnter2D(Collision2D other)
@@ -53,21 +57,27 @@ namespace Middle_Age_2D_Game
         /// </summary>
         private IEnumerator Run()
         {
-            
+
             x = Input.GetAxis("Horizontal"); // X축 이동
             transform.position += new Vector3(x, 0, 0) * moveSpeed * Time.deltaTime;
 
-            if (x > 0)
+            if (isGround == true) // 땅에 닿아있을 때
             {
-                knightAnim.SetBool("Run", true);
-                knightAnim.SetBool("Idle", false);
-                yield return null;
+                if (x != 0) //움직일 때
+                {
+                    transform.localScale = (x > 0) ? new Vector3(1,1,1) : new Vector3(-1,1,1); // 캐릭터 방향, 콜라이더 함께 좌,우 방향제어
+                    
+                    knightAnim.SetBool("Run", true);
+                    knightAnim.SetBool("Idle", false);
+
+                }
+                else //멈춰있을 때
+                {
+                    knightAnim.SetBool("Run", false);
+                    knightAnim.SetBool("Idle", true);
+                }
             }
-            else
-            {
-                knightAnim.SetBool("Run", false);
-                knightAnim.SetBool("Idle", true);
-            }
+            yield return null;
         }
 
         /// <summary>
@@ -77,7 +87,8 @@ namespace Middle_Age_2D_Game
         {
             if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
             {
-                 knightRb.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
+                knightRb.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
+                knightAnim.SetTrigger("Jump");
             }
         }
 
@@ -86,7 +97,10 @@ namespace Middle_Age_2D_Game
         /// </summary>
         void Attack()
         {
-
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                knightAnim.SetTrigger("Attack");
+            }
         }
     }
 }
