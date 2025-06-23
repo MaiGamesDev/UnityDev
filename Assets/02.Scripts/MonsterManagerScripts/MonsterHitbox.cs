8779dd1ddce4709a09834645e4af2f71a4e56f88
sbc605 <sbc605@naver.com>
2025-06-23 20:42:17 +0900

    몬스터 콜라이더 크기, 레이어순서 변경

--- Assets/02.Scripts/MonsterManagerScripts/MonsterHitbox.cs
+++ Assets/02.Scripts/MonsterManagerScripts/MonsterHitbox.cs
@@ -15,21 +15,13 @@ public class MonsterHitbox : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("히트박스 충돌 감지: " + other.name);
-
-        if (owner == null)
-        {
-            Debug.LogError("owner가 설정되지 않았습니다!");
-            return;
-        }
-
         if (other.CompareTag("Player"))
         {
             var player = other.GetComponent<TestPlayerMoveAttack>();
             if (player != null)
             {
                 Debug.Log("몬스터 공격력: " + owner.AttackDamage());
-                player.TakeDamage(owner.AttackDamage()); // 몬스터의 공격력 사용
+                player.TakeDamage(owner.AttackDamage()); // 연결된 MonsterManager에서 공격력 사용
             }
         }
     }
