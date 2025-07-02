29c1aa653d9acf5d8ea385433930ae6c29bba4ad
KSY <xaesoo30@gmail.com>
2025-07-03 00:00:31 +0900

    보스 FSM 초기 작업

--- Assets/SangYoon/02. Script/Boss/Boss Slime Demon.cs
+++ Assets/SangYoon/02. Script/Boss/Boss Slime Demon.cs
@@ -1,16 +1,80 @@
 using UnityEngine;
 
-public class BossSlimeDemon : MonoBehaviour
+public class BossSlimeDemon : MonoBehaviour, IBossDefaultPattern
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum SlimeDemon { IDLE, PATROL, TRACE, ATTACK, HIT, DEATH, SKILLS}
+    public SlimeDemon slimeDemonState = SlimeDemon.IDLE;
+
+
+    public float hp { get; set; }
+    public float attackDamage { get; set; }
+    public float moveSpeed { get; set; }
+
+    protected virtual void Init(float hp, float attackDamage, float moveSpeed)
     {
-        
+       this.hp = hp;
+       this.attackDamage = attackDamage;
+       this.moveSpeed = moveSpeed;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        switch (slimeDemonState)
+        {
+            case SlimeDemon.IDLE:
+                Idle();
+                break;
+            case SlimeDemon.PATROL:
+                break;
+            case SlimeDemon.TRACE:
+                break;
+            case SlimeDemon.ATTACK:
+                break;
+            case SlimeDemon.HIT:
+                break;
+            case SlimeDemon.DEATH:
+                break;
+            case SlimeDemon.SKILLS:
+                break;
+
+        }
+    }
+
+    public void Idle()
+    {
+
+    }
+    public void Death()
+    {
+
+    }
+
+    public void DefaultAttack()
+    {
+
+    }
+
+    public void Hit()
+    {
+
+    }
+
+
+    public void Walk()
+    {
+
+    }
+
+
+
+
+
+    private Rigidbody2D demonRb;
+    private Animator animator;
+
+    private void Start()
+    {
+        demonRb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 }
