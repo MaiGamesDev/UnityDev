51ae54cc6dd7d312b4420acd2a6428468bf75e24
KSY <xaesoo30@gmail.com>
2025-09-04 00:27:56 +0900

    FSM 패턴 구조 전환
    walk, attack, summon, idle, skill1,2,3 상태 전부 꼬여서 상태 전이 가 일어나며 동작함.
    
    FSM 상태 패턴을 사용해야만 해결이 가능하다는 것을 뼈저리게 느낌

--- Assets/SangYoon/02. Script/Boss/Boss Demon.cs
+++ Assets/SangYoon/02. Script/Boss/Boss Demon.cs
@@ -1,15 +1,31 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
-public class BossDemon : MonoBehaviour
+public class BossDemon : MonoBehaviour, IBossDefaultPattern
 {
-    public enum Demon { IDLE, Walk}
-    public Demon DemonState = Demon.IDLE;
+    public enum BossState { Idle, Walk, DefaultAttack, }
+    public BossState bossState;
 
-    public float hp = 500f;
-    public float defaultAttackDamage = 10f;
-    public float moveSpeed = 1.4f;
+    public float hp { get; set; } = 500f;
+    public float attackDamage { get; set; } = 10f;
+    public float moveSpeed { get; set; } = 1.4f;
     public float skillDamage { get; private set; }
+
+    public bool isIdle = true;
+    public bool isWalk = true;
+    public bool isAttack = true;
+
+    private float attackDist = 3.85f;
+
+    public float idleTime = 1f;
+    private float walkTime = 3f;
+    private float attackTime = 1f;
+
+    private float lastAttackEndTime = Mathf.NegativeInfinity; // -infinity로 언제든 일반 공격이 가능하게끔 하기위함
+    private float stateTimer;
 
     public Animator anim;
     private Rigidbody2D DemonRb;
@@ -17,12 +33,12 @@ public class BossDemon : MonoBehaviour
 
     [SerializeField] private Transform target;
 
-    protected virtual void Init(float hp, float attackDamage, float moveSpeed)
-    {
-       this.hp = hp;
-       this.defaultAttackDamage = attackDamage;
-       this.moveSpeed = moveSpeed;
-    }
+    //protected virtual void Init(float hp, float attackDamage, float moveSpeed)
+    //{
+    //    this.hp = hp;
+    //    this.attackDamage = attackDamage;
+    //    this.moveSpeed = moveSpeed;
+    //}
 
     private void Awake()
     {
@@ -38,31 +54,121 @@ public class BossDemon : MonoBehaviour
 
     void Update()
     {
-        switch (DemonState)
+        StateTransitions();
+
+        switch (bossState)
         {
-            case Demon.IDLE:
+            case BossState.Idle:
                 Idle();
                 break;
-            case Demon.Walk:
+
+            case BossState.Walk:
                 Walk();
+                break;
+
+            case BossState.DefaultAttack:
+                DefaultAttack();
                 break;
         }
     }
 
+    void StateTransitions()
+    {
+        float dist = Vector2.Distance(transform.position, target.position); // 공격 범위를 판별하기 위함
 
+        bool canAttack = (Time.time - lastAttackEndTime) >= attackTime;
+
+        if (target != null && dist <= attackDist && canAttack && bossState != BossState.DefaultAttack)
+        {
+            ChangeState(BossState.DefaultAttack);
+            return;
+        }
+        if (target != null && dist > attackDist && bossState != BossState.Walk && bossState != BossState.DefaultAttack)
+        {
+            ChangeState(BossState.Walk);
+            //stateTimer = Time.deltaTime; // 유사시 바로 사용할거라 주석 처리해두었음
+            return;
+        }
+        if (target != null && stateTimer >= walkTime && bossState != BossState.Idle && bossState != BossState.DefaultAttack)
+        {
+            ChangeState(BossState.Idle);
+            //stateTimer = 0; // 유사시 바로 사용할거라 주석 처리해두었음
+            return;
+        }
+    }
+
+    public void ChangeState(BossState newState)
+    {
+        if (bossState != newState)
+            bossState = newState;
+    }
     public void Idle()
     {
+        LookTarget();
+        anim.SetBool("isWalk", false);
+        anim.SetBool("isAttack", false);
         anim.SetTrigger("Idle");
+
+        stateTimer += Time.deltaTime;
+        if (stateTimer >= idleTime)
+        {
+            stateTimer = 0;
+        }
     }
 
     public void Walk()
     {
-        if (anim == null)
-            return;
+        LookTarget();
 
+        // 움직이는 로직
         Vector3 dir = (target.position - transform.position).normalized;
         transform.position += dir * moveSpeed * Time.deltaTime;
 
         anim.SetBool("isWalk", true);
+        anim.SetBool("isAttack", false);
+
+        stateTimer += Time.deltaTime;
+    }
+
+
+    public void DefaultAttack()
+    {
+        LookTarget();
+        anim.SetBool("isWalk", false);
+        anim.SetBool("isAttack", true);
+
+        stateTimer += Time.deltaTime;
+        if (stateTimer >= attackTime)
+        {
+            anim.SetBool("isAttack", false);
+            lastAttackEndTime = Time.time;
+
+            ChangeState(BossState.Idle);
+        }
+    }
+    //IEnumerator AttackRoutine()
+    //{
+
+    //}
+
+    public void Hit(float damage)
+    {
+
+    }
+
+    public void Death()
+    {
+
+    }
+
+    /// <summary>
+    /// 보스 방향 전환
+    /// </summary>
+    private void LookTarget()
+    {
+        Vector3 product = target.position - transform.position;
+        Vector3 scale = transform.localScale;
+        scale.x = product.x > 0 ? -1 : 1;
+        transform.localScale = scale;
     }
 }
--- Assets/SangYoon/02. Script/Boss/Demon Summon.cs
+++ Assets/SangYoon/02. Script/Boss/Demon Summon.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class DemonSummon : MonoBehaviour
@@ -11,13 +12,21 @@ public class DemonSummon : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        SummonTriggerEnter(other);
+        StartCoroutine(SummonTriggerEnterRoutine(other));
     }
-    private void SummonTriggerEnter(Collider2D other)
+    private IEnumerator SummonTriggerEnterRoutine(Collider2D other)
     {
+        bossDemon.isIdle = false;
+        bossDemon.isWalk = false;
+        bossDemon.isAttack = false;
+
         if (other.CompareTag("Player"))
         {
             bossDemon.anim.enabled = true;
+            yield return new WaitForSeconds(2f);
+
+            bossDemon.isIdle = true;
+            bossDemon.bossState = BossDemon.BossState.Idle;
             gameObject.SetActive(false);
         }
     }
